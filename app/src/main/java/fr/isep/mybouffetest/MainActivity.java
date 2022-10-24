f2b33df1d8a4603b12636d0b289a43ce00d01523
solisola1 <105809336+solisola1@users.noreply.github.com>
2022-10-24 22:17:34 +0200

    ajout du menu sans le calendrier

--- app/src/main/java/fr/isep/mybouffetest/MainActivity.java
+++ app/src/main/java/fr/isep/mybouffetest/MainActivity.java
@@ -1,9 +1,12 @@
 package fr.isep.mybouffetest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -25,5 +28,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.exemple_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                return true;
+            case R.id.item2:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
